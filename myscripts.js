1328b20b324875265541b9f93da5687757237908
sherline deguzman <sherlindeguzman@student.laverdad.edu.ph>
2022-11-17 20:36:29 +0800

    new update

---
+++ myscripts.js
@@ -0,0 +1,9 @@
+// Family picture (ABOUT ME page)
+
+function myFunction(imgs) {
+    var expandImg = document.getElementById("expandedImg");
+    var imgText = document.getElementById("imgtext");
+    expandImg.src = imgs.src;
+    imgText.innerHTML = imgs.alt;
+    expandImg.parentElement.style.display = "block";
+  }
